83cca62dab5fcf5ea2f65f4fe9269fbbdc5f267f
Daviwolf <79711924+Daviwolf@users.noreply.github.com>
2021-12-29 01:33:13 +0000

    Add files via upload

---
+++ Ecra_principal.java
@@ -0,0 +1,14 @@
+package com.example.eros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ecra_principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ecra_principal);
+    }
+}
