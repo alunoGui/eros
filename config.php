a04bfb6b7aced2c56e6cf0cc431fd4e7ba28b39b
Daviwolf <79711924+Daviwolf@users.noreply.github.com>
2022-01-17 09:42:01 +0000

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('Servidor nodejs', 'localhost:8000');
-   define('username', 'root');
-   define('password', 'rootpassword');
-   define('EROS_BD', 'database');
-   $db = mysqli_connect(Servidor nodejs,username,password,EROS_BD);
-?>
